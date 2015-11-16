a12c3e2414aa53b8fede7fcdcd6a4200a2be2415
Stas S <stormtamb@yandex.ru>
2015-11-17 02:20:41 +0300

    exec select

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
 db = SQLite3::Database.new 'test1.sqlite'
 
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+end
 
 db.close
